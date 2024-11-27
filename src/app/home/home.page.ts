1cf56dc99c570daa15da9fd457a46cfd489d95ba
sBohTor <sbohtor073@g.educaand.es>
2024-11-27 13:25:45 +0100

    Actividad1.9

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  sueldo: number = 0;
+  apuesta: number = 0;
+  saldo: number = 0;
+  jugando: boolean = false;
+  resultado: string = '';
+  mostrarRuleta: boolean = false;
 
   constructor() {}
 
+  iniciarJuego() {
+    this.saldo = this.sueldo;
+    this.mostrarRuleta = false;
+  }
+
+  hacerApuesta() {
+    if (this.apuesta > 0 && this.apuesta <= this.saldo) {
+      this.jugando = true;
+      this.mostrarRuleta = true;
+      this.resultado = '';
+
+    
+      setTimeout(() => {
+        this.mostrarRuleta = false;
+        this.jugar();
+      }, 2000);
+    }
+  }
+
+  jugar() {
+    const gano = Math.random() < 0.5; 
+    if (gano) {
+      this.saldo += this.apuesta *2-this.apuesta; 
+      this.resultado = '¡Ganaste!';
+    } else {
+      this.saldo -= this.apuesta; 
+      this.resultado = 'Perdiste.';
+    }
+    this.jugando = false;
+    this.mostrarRuleta = false;
+  }
 }
